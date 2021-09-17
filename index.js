62966e96a65ce7e5d8a58c48c65b10e1713b1ce1
MrBartusek <bartusekcraft@gmail.com>
2021-09-17 21:27:48 +0200

    New look!

--- index.js
+++ index.js
@@ -2,79 +2,89 @@ const Mustache = require('mustache');
 const config = require('./config.json')
 const fs = require('fs');
 const fetch = require('node-fetch');
+const technologyShields = require('technology-shields')
 const MUSTACHE_MAIN_DIR = './main.mustache';
-process.on('unhandledRejection', (e) => { throw e});
 
-let fields = {
-   refresh_date: new Date().toLocaleDateString('en-GB', {
-      weekday: 'long',
-      month: 'long',
-      day: 'numeric',
-      hour: 'numeric',
-      minute: 'numeric',
-      timeZoneName: 'short',
-      timeZone: 'Europe/Warsaw',
-   }),
-   total_projects: 0,
-   total_issues: 0,
-   total_pull_requests: 0,
+async function getProjects(config)
+{
+   let result = [];
+   for(const projectName of config.projects)
+   {
+      console.log(`Fetching ${projectName}`)
+      const projectData = await callApi(`https://api.github.com/repos/${projectName}`)
+      let project = {}
+      project.stars = projectData.stargazers_count
+      project.name = projectData.name
+      project.url = projectData.html_url
+      project.description = projectData.description
+      result.push(project)
+   }
+   result = result.sort((a, b) => (a.stars < b.stars) ? 1 : -1)
+   return result
 }
 
-async function getContributedProjects()
+function getTechnologies()
 {
-   console.log('Getting projects contributed list...')
-   let lastResLength = 0, i = 0, result = [];
-   do
-   {
-      let res = await fetch('https://api.github.com/search/issues?q=author:MrBartusek&per_page=100&page=' + (i + 1),
-      { headers: { 'Authorization' : 'Basic ' + Buffer.from("MrBartusek" + ":" + process.env.GITHUB_PERSONAL_TOKEN).toString('base64') } })
-      if(!res.ok)
-      {
-         res = await res.json();
-         console.log(res);
-         throw new Error('Contributed Projects API Error');
-      }
-      else
-      {
-         res = await res.json();
-         console.log(`- Got page ${i+1} with ${res.items.length} results | Total search size: ${res.total_count}`)
-         lastResLength = res.items.length;
-         i++;
-         for(const issue of res.items)
-         {
-            if(issue.author_association == "MEMBER") continue;
-            const repositoryName = issue.repository_url.replace('https://api.github.com/repos/','');
-            let index = result.findIndex(x => x.name == repositoryName);
-            if(index == -1)
-            {
-               fields.total_projects++;
-               index = result.push({ name: repositoryName, issues: 0, pull_requests: 0 }) - 1;
-            }
-            if(issue.draft != undefined)
-            {  
-               fields.total_pull_requests++;
-               result[index].pull_requests++; 
-            }
-            else
-            { 
-               fields.total_issues++;
-               result[index].issues++; 
-            }
-         }
-      }
-   } while(lastResLength == 100)
-   result = result.sort((a,b) => (b.issues*0.25 + b.pull_requests)-(a.issues*0.25 + a.pull_requests));
-   result = result.filter((x) => (x.issues *0.25 + x.pull_requests) > 1);
-   result = result.slice(0,8);
-   console.log(`- Done. Total projects: ${fields.total_projects} Total issues: ${fields.total_issues} Total PRs ${fields.total_pull_requests}`)
-   return result;
+   return technologyShields.get([
+      // JS
+      "typescript",
+      "javascript",
+      "nodedotjs",
+      "npm",
+
+      // PYTHON
+      "python",
+      "pypi",
+      "readthedocs",
+
+      // DATABASE
+      "sqlite",
+      "postgresql",
+
+      // HOSTING
+      "heroku",
+      "pm2",
+
+      // WEB
+      "html5",
+      "react",
+
+      // OTHER
+      "githubactions",
+      "eslint",
+      "git",
+      "visualstudiocode",
+      "discord",
+      "premid",
+      "homeassistant",
+      "vivaldi",
+      "arduino",
+      "raspberrypi"
+   ], "MARKDOWN")
+}
+
+function calculateAge()
+{
+   // 🎂
+   var ageDifMs = Date.now() - new Date(2003, 2, 14).getTime();
+   var ageDate = new Date(ageDifMs);
+   return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+
+async function callApi(endpoint)
+{
+   let res = await fetch(endpoint, {headers:{'Authorization' : 'Basic ' + Buffer.from("MrBartusek" + ":" + process.env.GITHUB_PERSONAL_TOKEN).toString('base64')}})
+   return await res.json();
 }
 
 async function main()
 {
    console.log('Starting readme generation...')
-   const contributedProjects = await getContributedProjects();
-   fields = {...config, ...fields, open_source_contributions: contributedProjects}
+   const fields = {
+      projects: await getProjects(config),
+      technologies: getTechnologies(),
+      age: calculateAge()
+   }
 
    console.log('Generating template...')
    fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
